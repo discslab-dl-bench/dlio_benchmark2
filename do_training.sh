ed713bee5ded7e28dac8d6025aaaba02e067b9b6
jovonho <loic.hovon@gmail.com>
2022-12-13 09:30:15 -0500

    make launchable for tracing

--- do_training.sh
+++ do_training.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 WORKLOAD=${1:-bert}
 NUM_GPUS=${2:-4}
-BATCH_SIZE=
+BATCH_SIZE=${3:-6}
 
-mpirun -np 8 $NUM_GPUS src/dlio_benchmark.py workload=$WORKLOAD ++workload.workflow.profiling=True ++workload.profiling.profiler=iostat ++workload.profiling.iostat_devices=[sda,sdb]
+mpirun -np $NUM_GPUS python3 src/dlio_benchmark.py workload=$WORKLOAD ++workload.reader.batch_size=$BATCH_SIZE ++workload.workflow.profiling=True ++workload.profiling.profiler=iostat ++workload.profiling.iostat_devices=[sda,sdb]
--- start_training.sh
+++ start_training.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-WORKLOAD=${1:-bert}
 
-docker run -it --rm -v /raid/data/dlio:/workspace/dlio/data dlio:loic /bin/bash training.sh $WORKLOAD
+NUM_GPUS=${1:-4}
+CONTAINER_NAME=${2:-train_dlio}
+WORKLOAD=${3:-bert}
+BATCH_SIZE=${4:-4}
+
+docker run -it --rm --name $CONTAINER_NAME -v /raid/data/dlio:/workspace/dlio/data dlio:loic /bin/bash do_training.sh $WORKLOAD $NUM_GPUS $BATCH_SIZE
