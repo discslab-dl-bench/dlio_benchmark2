f677249d5db656aa538c09b6cc69dd03398c0226
jovonho <loic.hovon@gmail.com>
2023-02-20 18:06:37 -0500

    DLRM sim

--- exec.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mpirun -np 8 python3 src/dlio_benchmark.py workload=dlrm
-cp -r /workspace/dlio/hydra_log/dlrm/* /workspace/dlio/save_spot
--- run_dlio2.sh
+++ run_dlio2.sh
@@ -10,10 +10,11 @@ fi
 
 # docker run -it --rm --name=$container_name --gpus all -v /raid/data/dlrm_dlio2/dlio2:/workspace/dlio/data/dlrm dlio:latest /bin/bash exec.sh
 sudo docker run -it \
---rm \
---name=$container_name \
---gpus all \
--v /raid/data/dlrm_dlio2/dlio2:/workspace/dlio/data/dlrm \
--v /raid/data/dlrm_dlio2/dlio2_log:/workspace/dlio/save_spot \
-dlio:latest \
-/bin/bash exec.sh
+	--rm \
+	--name=$container_name \
+	--gpus all \
+	-v /raid/data/dlrm_dlio2/dlio2:/workspace/dlio/data/dlrm \
+	-v /raid/data/dlio/run_output:/workspace/dlio/hydra_log \
+	-v /raid/data/dlio/run_output:/workspace/dlio/checkpoints \
+	dlio:latest \
+	/bin/bash exec.sh
