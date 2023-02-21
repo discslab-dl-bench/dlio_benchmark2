f031d1f146959d6319979066d825e3e47d9beab3
jovonho <loic.hovon@gmail.com>
2023-02-21 14:27:29 -0500

    DLRM sim pass in batch size and num gpus

--- do_training.sh
+++ do_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-WORKLOAD=${1:-bert}
+WORKLOAD=${1:-dlrm}
 NUM_GPUS=${2:-8}
-BATCH_SIZE=
+BATCH_SIZE=${3:-2048}
 
-mpirun -np $NUM_GPUS python3 src/dlio_benchmark.py workload=$WORKLOAD
+mpirun -np $NUM_GPUS python3 src/dlio_benchmark.py workload=$WORKLOAD ++workload.reader.batch_size=$BATCH_SIZE
--- start_training.sh
+++ start_training.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-NUM_GPUS=${1:-bert}
-CONTAINER_NAME=${2:-bert}
-WORKLOAD=${3:-bert}
+NUM_GPUS=${1:-8}
+CONTAINER_NAME=${2:-dlio_loic}
+WORKLOAD=${3:-dlrm}
+BATCH_SIZE=${4:-2048}
 
 docker run -it --rm --name $CONTAINER_NAME \
 	-v /raid/data/dlrm_dlio2/dlio2:/workspace/dlio/data/dlrm \
 	-v /raid/data/dlio/run_output:/workspace/dlio/hydra_log \
 	-v /raid/data/dlio/run_output:/workspace/dlio/checkpoints \
-    dlio:$WORKLOAD /bin/bash do_training.sh $WORKLOAD
+    dlio:$WORKLOAD /bin/bash do_training.sh $WORKLOAD $NUM_GPUS $BATCH_SIZE
