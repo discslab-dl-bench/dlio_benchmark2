8d043e1699e01406a3dd39d3d553d1f69c7312f8
jovonho <loic.hovon@gmail.com>
2023-02-21 23:01:46 -0500

    fix batch size

--- start_container.sh
+++ start_container.sh
@@ -2,8 +2,13 @@
 
 IMAGE=${1:-dlio:dlrm}
 
-docker run -it --rm \
-    -v /raid/data/dlrm_dlio2/dlio2:/workspace/dlio/data/dlrm \
-    -v /raid/data/dlio/run_output:/workspace/dlio/hydra_log \
-    -v /raid/data/dlio/run_output:/workspace/dlio/checkpoints \
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
+LOGGING_DIR="${SCRIPT_DIR}/output"
+
+mkdir -p $LOGGING_DIR
+
+docker run -it --rm --name dlrm_loic \
+	-v /raid/data/dlio/data:/workspace/dlio/data \
+	-v $LOGGING_DIR:/workspace/dlio/hydra_log \
+	-v /raid/data/dlio/run_output:/workspace/dlio/checkpoints \
     $IMAGE /bin/bash
