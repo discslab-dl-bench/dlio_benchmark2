d0faa2a64eb191a453e0ce07f7b4d0eb3922ae2c
jovonho <loic.hovon@gmail.com>
2023-02-28 15:34:48 -0500

    add number of steps as parameter

--- do_training.sh
+++ do_training.sh
@@ -2,5 +2,6 @@
 WORKLOAD=${1:-dlrm}
 NUM_GPUS=${2:-8}
 BATCH_SIZE=${3:-2048}
+NUM_STEPS=${4:-1000}
 
-mpirun -np 1 python3 src/dlio_benchmark.py workload=$WORKLOAD ++workload.reader.batch_size=$BATCH_SIZE ++workload.num_gpus=$NUM_GPUS
+mpirun -np 1 python3 src/dlio_benchmark.py workload=$WORKLOAD ++workload.reader.batch_size=$BATCH_SIZE ++workload.num_gpus=$NUM_GPUS ++workload.train.total_training_steps=$NUM_STEPS
