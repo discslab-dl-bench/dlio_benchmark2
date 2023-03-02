5aab8448a3dc4d85ed0d0d0ff2e8a6651315390c
jovonho <loic.hovon@gmail.com>
2023-03-01 19:40:12 -0500

    change start training script

--- start_training.sh
+++ start_training.sh
@@ -3,10 +3,10 @@
 NUM_GPUS=${1:-2}
 CONTAINER_NAME=${2:-dlio_loic}
 LOGGING_DIR=${3:-"/raid/data/dlio/run_output"}
-IMAGE=${4:-dlio:dlrm}
+IMAGE=${4:-dlio:dlrm-instru}
 WORKLOAD=${5:-dlrm}
 BATCH_SIZE=${6:-2048}
-NUM_STEPS=${7:-32768}
+NUM_STEPS=${7:-1000}
 
 docker run -it --rm --name $CONTAINER_NAME \
 	-v /raid/data/dlio/data:/workspace/dlio/data \
