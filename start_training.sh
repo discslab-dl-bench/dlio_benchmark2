b944ea427e3e3835ba3f8578ce54421515fa10ee
jovonho <loic.hovon@gmail.com>
2023-03-20 19:59:25 -0400

    add num epochs parameter to launch script

--- start_training.sh
+++ start_training.sh
@@ -6,9 +6,10 @@ LOGGING_DIR=${3:-"/raid/data/dlio/run_output"}
 IMAGE=${4:-dlio:unet3d-instru}
 WORKLOAD=${5:-unet3d}
 BATCH_SIZE=${6:-4}
+NUM_EPOCHS=${7:-50}
 
 docker run -it --rm --name $CONTAINER_NAME \
 	-v /raid/data/dlio/data:/workspace/dlio/data \
 	-v $LOGGING_DIR:/workspace/dlio/hydra_log \
 	-v /raid/data/dlio/run_output:/workspace/dlio/checkpoints \
-    $IMAGE /bin/bash do_training.sh $WORKLOAD $NUM_GPUS $BATCH_SIZE
+    $IMAGE /bin/bash do_training.sh $WORKLOAD $NUM_GPUS $BATCH_SIZE $NUM_EPOCHS
